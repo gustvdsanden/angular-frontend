156b92c0259966382edeacbd185a27d0b3897d79
gustvdsanden <gustvdsanden@gmail.com>
2021-08-08 13:49:44 +0200

    annoying bug in deployment

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../services/post.service';
-import { Post, User } from '../models/index';
+import { Post, User } from '../models';
 import { FormBuilder } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 @Component({
